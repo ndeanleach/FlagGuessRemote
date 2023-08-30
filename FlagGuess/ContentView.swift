abc168136c5e1ec33df487c2d1838a34ceb2ce0b
deannate00 <deannate00@gmail.com>
2023-08-30 19:21:26 -0400

    Changed content

--- FlagGuess/ContentView.swift
+++ FlagGuess/ContentView.swift
@@ -1,6 +1,6 @@
 //
 //  ContentView.swift
-//  FlagGuess
+//  FlagGuessGame
 //
 //  Created by Nathan Leach on 8/30/23.
 //
@@ -8,15 +8,85 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State private var playerScore = 0
+    @State private var showingScore = false
+    @State private var scoreTitle = ""
+    
+    @State private var countries = ["Estonia", "France", "Germany", "Ireland", "Italy", "Monaco", "Nigeria", "Poland", "Russia", "Spain", "UK", "US"].shuffled()
+    
+    @State private var correctAnswer = Int.random(in: 0...2)
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        ZStack{
+            RadialGradient(stops: [.init(color: Color(red: 0.1, green: 0.2, blue: 0.45), location: 0.3), .init(color: Color(red: 0.76, green: 0.15, blue: 0.26), location: 0.3)], center: .top, startRadius: 200, endRadius: 700)
+                .ignoresSafeArea()
+            VStack{
+                Spacer()
+                Text("Guess the Flag")
+                    .font(.largeTitle.weight(.bold))
+                    .foregroundColor(.white)
+                VStack(spacing: 15) {
+                    VStack{
+                        Text("Tap the flag of")
+                            .foregroundStyle(.secondary)
+                            .font(.subheadline.weight(.heavy))
+                        Text(countries[correctAnswer])
+                            .font(.largeTitle.weight(.semibold))
+                    }
+                    .padding(20)
+                    ForEach(0..<3) { num in
+                        Button{
+                            flagTapped(num)
+                        } label: {
+                            Image(countries[num])
+                                .clipShape(Capsule())
+                                .shadow(radius: 20)
+                        }
+                    }
+                }
+                .frame(maxWidth: .infinity)
+                .padding(.vertical, 20)
+                .background(.regularMaterial)
+                .clipShape(RoundedRectangle(cornerRadius: 20))
+                Spacer()
+                Spacer()
+                Text("Score: \(playerScore)")
+                    .foregroundColor(.white)
+                    .font(.title.bold())
+                Spacer()
+            }
+            .padding(40)
         }
-        .padding()
+        .alert(scoreTitle, isPresented: $showingScore){
+            Button("Continue", action: askQuestion)
+        } message: {
+            Text("Your score is \(playerScore)")
+        }
     }
+    
+    func flagTapped(_ number: Int){
+        if number == correctAnswer{
+            if playerScore < 10 {
+                scoreTitle = "Correct"
+                playerScore += 1
+                showingScore = true
+            } else{
+                scoreTitle = "You Win! Starting New Game"
+                showingScore = true
+                playerScore = 0
+            }
+        } else {
+            scoreTitle = "Wrong"
+            showingScore = true
+        }
+    }
+    
+    func askQuestion(){
+        countries.shuffle()
+        correctAnswer = Int.random(in: 0...2)
+    }
+    
 }
 
 struct ContentView_Previews: PreviewProvider {
@@ -24,3 +94,4 @@ struct ContentView_Previews: PreviewProvider {
         ContentView()
     }
 }
+
