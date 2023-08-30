57f5f94cde9af40e16e467a4e3a71bbddbc80a4f
deannate00 <deannate00@gmail.com>
2023-08-30 19:19:58 -0400

    Initial Commit

---
+++ FlagGuess/FlagGuessApp.swift
@@ -0,0 +1,17 @@
+//
+//  FlagGuessApp.swift
+//  FlagGuess
+//
+//  Created by Nathan Leach on 8/30/23.
+//
+
+import SwiftUI
+
+@main
+struct FlagGuessApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
